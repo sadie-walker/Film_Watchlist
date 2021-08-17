5ca3b4b764664a6b6fed51f9b7c14219dad4b781
sadie-walker <64597433+sadie-walker@users.noreply.github.com>
2021-08-17 15:34:15 +0100

    style scrollbar

--- src/ui.js
+++ src/ui.js
@@ -41,7 +41,7 @@ export const populateWatchlist = (watchlist) => {
                 <img src="https://image.tmdb.org/t/p/original/${film.poster_path}">
             </td>
             <td>${film.title} (${new Date(film.release_date).getFullYear()})</td>
-            <td class="watchlist-desc"><div>${film.overview}</div></td>
+            <td class="watchlist-desc pe-4"><div>${film.overview}</div></td>
             <td>
                 <div class="d-flex align-items-center justify-content-between">${getLocationList(film)}</div>
             </td>
